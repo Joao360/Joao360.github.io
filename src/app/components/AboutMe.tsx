048ab4be3c1870fc65de4758beb2e1a444cc4116
João Graça <joaograca360@gmail.com>
2024-10-20 12:39:35 +0100

    feat: translate content to english

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -16,8 +16,8 @@ const AboutMe = () => {
                 className="object-cover"
               />
             </div>
-            <h2 className="text-xl font-bold mb-2 text-black">João Graça</h2>
-            <p className="text-gray-500 mb-4">Android Engineer</p>
+            <h2 className="text-xl font-bold mb-2 text-black">{"João Graça"}</h2>
+            <p className="text-gray-500 mb-4">{"Android Engineer"}</p>
             <div className="w-12 h-1 bg-blue-600 mb-4"></div>
             <div className="flex space-x-4">
               <a href="#" className="text-gray-600 hover:text-blue-500"><i className="fab fa-facebook"></i></a>
@@ -31,15 +31,14 @@ const AboutMe = () => {
 
       {/* Right Section (Main Content) */}
       <div className="w-2/3 p-12 flex flex-col justify-center bg-white">
-        <h1 className="text-6xl font-bold mb-4 text-black">Olá</h1>
-        <p className="text-xl font-light mb-8 text-black">Meu perfil e meus projetos</p>
+        <h1 className="text-6xl font-bold mb-4 text-black">{"Hey!"}</h1>
+        <p className="text-xl font-light mb-8 text-black">{"My profile and my projects"}</p>
         <div className="flex space-x-4 mb-8">
-          <a href="#curriculo" className="px-6 py-3 bg-blue-800 text-white rounded hover:bg-blue">Currículo</a>
-          <a href="#projetos" className="px-6 py-3 border border-gray-300 text-gray-700 rounded hover:bg-gray-100">Projetos</a>
+          <a href="#curriculo" className="px-6 py-3 bg-blue-800 text-white rounded hover:bg-blue">{"Experience"}</a>
+          <a href="#projetos" className="px-6 py-3 border border-gray-300 text-gray-700 rounded hover:bg-gray-100">{"Projects"}</a>
         </div>
         <p className="text-gray-600 max-w-md">
-          Sou um Android Engineer apaixonado por desenvolver aplicações móveis que transformam ideias em realidade.
-          Sempre buscando simplicidade e impacto nos meus projetos.
+          {"Passionate about mobile app development, I've honed my skills in Android app development and led a small mobile team on a React Native project. With a solid foundation in native frameworks, I've successfully delivered top-notch mobile applications."}
         </p>
       </div>
     </section>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer id="contact" className="py-10 flex items-center justify-center bg-gray-200">
             <div>
-                <p className="text-black">&copy; 2024 João Graça. Todos os direitos reservados.</p>
+                <p className="text-slate-500">{"© 2024 João Graça. All rights reserved."}</p>
             </div>
         </footer>
     )
