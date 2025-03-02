528fdb989e1808e00af5edcc2624bd12579ceebb
João Graça <joaograca360@gmail.com>
2025-03-02 12:45:26 +0000

    feat: remove year from copy right and add favicon

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer id="contact" className="py-10 flex items-center justify-center">
             <div>
-                <p className="text-slate-500">{"© 2024 João Graça. All rights reserved."}</p>
+                <p className="text-slate-500">{"© João Graça. All rights reserved."}</p>
             </div>
         </footer>
     )
