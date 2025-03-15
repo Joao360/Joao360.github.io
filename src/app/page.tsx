c960ace1a0b86b05ab4a0b0f2612b1b2b32179ab
João Graça <joaograca360@gmail.com>
2025-03-15 16:29:40 +0000

    refactor: move components package to src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // pages/index.js
 
 import React from 'react';
-import NavigationBar from './components/NavigationBar';
-import Experience from './components/Experience';
-import AboutMe from './components/AboutMe';
-import Footer from './components/Footer';
+import NavigationBar from '../components/NavigationBar';
+import Experience from '../components/Experience';
+import AboutMe from '../components/AboutMe';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
