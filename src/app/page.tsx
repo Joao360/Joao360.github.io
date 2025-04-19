2dd89f3228ed036715afea941ea6785417c06a1c
João Graça <joaograca360@gmail.com>
2025-04-19 10:31:24 +0100

    #2: Add a contact form (#15)
    * refactor: move components package to src
    
    * chore: install react-hook-form
    
    * refactor: use main tag instead of div
    
    * feat: implement contact form UI
    
    * chore: install nodemailer
    
    * refactor: remove duplicated component
    
    * refactor: justify center the contact form
    
    * feat: create email endpoint skeleton
    
    * chore: indentation
    
    * feat: implement success screen and add header to the contact form
    
    * feat: improve form visually
    
    * refactor: remove unnecessary code and dependencies
    
    * feat: implement work around for netlify forms
    
    * fix: compilation error
    
    * fix: add method to the form html source
    
    * feat: add an alert to display the status of the contact request
    
    * refactor: Remove github pages action
    
    * fix: convert formData to record
    
    * fix: static form
    
    * fix: use correct form name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 // pages/index.js
 
 import React from 'react';
-import NavigationBar from './components/NavigationBar';
-import Experience from './components/Experience';
-import AboutMe from './components/AboutMe';
-import Footer from './components/Footer';
+import NavigationBar from '../components/NavigationBar';
+import Experience from '../components/Experience';
+import AboutMe from '../components/AboutMe';
+import Footer from '../components/Footer';
+import Contact from '@/components/Contact';
 
 const Home = () => {
   return (
-    <div className='bg-gray-100'>
+    <main className='bg-gray-100'>
       <NavigationBar />
       <AboutMe />
       <Experience />
+      <Contact />
       <Footer />
-    </div>
+    </main>
   );
 };
 
---
+++ src/components/Contact.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { FC, useState } from 'react';
+import { Alert } from './Alert';
+
+const Contact: FC = () => {
+  const [status, setStatus] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmitForm = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      setStatus('pending');
+      setError(null);
+      
+      const formObject: Record<string, string> = {};
+      const formData = new FormData(event.currentTarget);
+      formData.forEach((value, key) => {
+        formObject[key] = value.toString();
+      });
+      
+      const res = await fetch(
+        "/_forms.html",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formObject).toString(),
+        }
+      );
+
+      if (res.status === 200) {
+        setStatus('ok');
+      } else {
+        setStatus('error');
+        setError(`${res.status} ${res.statusText}`);
+      }
+    } catch (error) {
+      setError(`${error}`);
+    }
+  };
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='mb-10 mt-10 flex w-full flex-col items-center justify-center'>
+        <h1 className='mb-5 text-center text-4xl font-bold text-blue-950'>
+          Have a Question or Idea?
+        </h1>
+        <p className='mb-5 text-center text-lg font-normal text-gray-600'>
+          I’m just a message away! Whether you want to discuss a potential collaboration or simply have a question, reach out below — I’d be happy to connect.
+        </p>
+      </div>
+
+      <form
+        name='feedback'
+        className='w-full max-w-lg bg-white rounded-lg shadow-lg p-8'
+        onSubmit={onSubmitForm}
+      >
+        <input type='hidden' name='form-name' value='contact' />
+
+        <div className='mb-5'>
+          <label
+            htmlFor='name'
+            className='mb-3 block text-base font-medium text-gray-700'
+          >
+            Your Name
+          </label>
+
+          <input
+            type='text'
+            name='name'
+            placeholder='Full Name'
+            className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 transition duration-200'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label
+            htmlFor='email'
+            className='mb-3 block text-base font-medium text-gray-700'
+          >
+            Email Address
+          </label>
+
+          <input
+            type='email'
+            name='email'
+            placeholder='example@domain.com'
+            className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 transition duration-200'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label
+            htmlFor='message'
+            className='mb-3 block text-base font-medium text-gray-700'
+          >
+            Message
+          </label>
+
+          <textarea
+            name='message'
+            rows={4}
+            placeholder='Type your message'
+            className='w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500 transition duration-200'
+          ></textarea>
+        </div>
+
+        <div className='flex justify-center'>
+          <button
+            className='hover:shadow-lg hover:bg-blue-600 transform hover:scale-105 transition duration-300 rounded-md bg-blue-500 py-3 px-8 text-base font-semibold text-white outline-none'
+            disabled={status === 'pending'}
+          >
+            Submit
+          </button>
+        </div>
+
+        {status === 'pending' && (
+          <div className='mt-5 text-sm text-gray-600'>
+            Please wait...
+          </div>
+        )}
+        {status === 'ok' && <Alert type="success" className='mt-5'>Submitted!</Alert>}
+        {status === 'error' && <Alert type="error" className='mt-5'>{error}</Alert>}
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
